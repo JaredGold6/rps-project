f046c87c4fe6f245308c1b0d0fe65966c4ff5ae9
JaredGold6 <170373955+JaredGold6@users.noreply.github.com>
2024-08-02 12:05:13 -0400

    Finish game and correct return for inputs

--- rps.js
+++ rps.js
@@ -1,14 +1,18 @@
+//Variables for inputs
+let humanAnswer
+let computerAnswer
+
 //Create function to randomize rock, paper, scissors on conputer side
 function getComputerChoice() {
     let randInt = Math.floor(Math.random() * 3);
     if (randInt === 0) {
-        return `rock`
+        return computerAnswer = `rock`
     }
     else if (randInt === 1) {
-        return `paper`
+        return computerAnswer = `paper`
     }
     else if (randInt === 2) {
-        return `scissors`
+        return computerAnswer = `scissors`
     }
 }
 
@@ -17,7 +21,7 @@ function getComputerChoice() {
     for (let i = 0; i < 2;) {
         let answer = prompt(`Enter Rock, Paper, or Scissors`).toLowerCase()
         if (answer === `rock` || answer === `paper` || answer === `scissors`) {
-            return answer
+            return humanAnswer = answer
         }
         else if (answer === `shoot`) {
             alert(`alright funny man`)
@@ -29,13 +33,11 @@ function getComputerChoice() {
  } 
 
 
- //save choices
 
+
+//save choices
  const humanSelection = getHumanChoice()
  const computerSelection = getComputerChoice()
-
- console.log(`Human chose ${humanSelection}`)
- console.log(`Computer chose ${computerSelection}`)
 
 //keep track of players score and computers score
 let humanScore = 0
@@ -62,9 +64,29 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-playRound(humanSelection, computerSelection)
+
 
 console.log(humanScore)
 console.log(computerScore)
 
 //function for playing a game
+
+function playGame() {
+    while (humanScore < 3 && computerScore < 3) {
+        playRound(humanSelection, computerSelection)
+        alert(`Score is currently Player: ${humanScore} Computer: ${computerScore}`)
+        if (humanScore < 3 && computerScore <3) {
+            getComputerChoice()
+            getHumanChoice()
+        }
+    }
+
+    if (humanScore > computerScore) {
+        alert(`Congratualtions! you win!`)
+    }
+    else {
+        alert(`You lose!`)
+    }
+}
+
+playGame()
