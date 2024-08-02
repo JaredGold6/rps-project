918c6954b24112771d8a4a60c3454f2def98c87e
JaredGold6 <170373955+JaredGold6@users.noreply.github.com>
2024-08-02 10:39:38 -0400

    Add function and logic for playing a round

--- rps.js
+++ rps.js
@@ -12,8 +12,6 @@ function getComputerChoice() {
     }
 }
 
-console.log(`Computer chose ${getComputerChoice()}`)
-
 //Create function to save players input
  function getHumanChoice() {
     for (let i = 0; i < 2;) {
@@ -30,12 +28,43 @@ console.log(`Computer chose ${getComputerChoice()}`)
     }
  } 
 
- console.log(`Human chose ${getHumanChoice()}`)
+
+ //save choices
+
+ const humanSelection = getHumanChoice()
+ const computerSelection = getComputerChoice()
+
+ console.log(`Human chose ${humanSelection}`)
+ console.log(`Computer chose ${computerSelection}`)
 
 //keep track of players score and computers score
 let humanScore = 0
 let computerScore = 0
 
 //function for playing a round
+function playRound(humanChoice, computerChoice) {
+    for (let i = 0; i < 2;) {
+        if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
+            (humanChoice === 'paper' && computerChoice === 'rock') ||
+            (humanChoice === 'scissors' && computerChoice === 'paper')) {
+                humanScore++
+                return alert(`You win!, ${humanChoice} beats ${computerChoice}!`)
+            }
+        else if (humanChoice === computerChoice) {
+            alert(`Tie! Try again`)
+            computerChoice = getComputerChoice()
+            humanChoice = getHumanChoice()
+        }
+        else {
+            computerScore++
+            return alert(`You lose!, ${computerChoice} beats ${humanChoice}!`)
+        }
+    }
+}
+
+playRound(humanSelection, computerSelection)
+
+console.log(humanScore)
+console.log(computerScore)
 
 //function for playing a game
